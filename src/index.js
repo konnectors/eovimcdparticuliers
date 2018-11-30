2b4a9546a27693d2dfe98f2da8dd2091b239c30d
LucsT <lucas@cozycloud.cc>
2018-11-30 17:31:08 +0100

    feat: Adding a generated pdf as fallback

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const {
   createCozyPDFDocument
 } = require('cozy-konnector-libs')
 
+const cheerio = require('cheerio')
 const request = requestFactory({
 //  debug: true,
   cheerio: true,
@@ -32,7 +33,6 @@ async function start(fields) {
   try {
     pdfList = scrapePdfList($pdf)
     log('info', `Found ${pdfList.length} pdfs, expect as many masterline`)
-    console.log('pdf', pdfList)
   } catch(e) {
     log('warn', e)
     log('warn', 'Impossible to fetch pdfs, option may be not activated')
@@ -43,11 +43,11 @@ async function start(fields) {
 
   log('info', 'Parsing list of refunds')
   const bills = await parseBills($, pdfList)
+  log('info', `${bills.length} bills generated`)
 
-  console.log('bills', bills)
   log('info', 'Saving data to Cozy')
   await saveBills(bills, fields, {
-    identifiers: ['todoeovimcd'] //TODO  FOUND IDENTIFIERS
+    identifiers: ['EOVI MCD MUTUELLE']
   })
 }
 
@@ -71,75 +71,97 @@ function authenticate(username, password) {
 async function parseBills($, pdfList) {
   const bills = []
   // Forced to use collapsed class of this tr because ligne-titre is in sub table
-  //Note 1er await useless
-  await $('tbody tr[class="ligne-titre collapsed"]').each(async (index, el) => {
+  const masters = Array.from($('tbody tr[class="ligne-titre collapsed"]'))
+  for (let index=0; index<masters.length; index++) {
+    const el = masters[index]
     log('info', `Found master line number ${index}`)
     // Verify next tr is a line with sub bills
-    if($(el).next('tr').hasClass('ligne-detail') !== true) {
+    if ($(el).next('tr').hasClass('ligne-detail') !== true) {
       log('error', "Next tr hasn't ligne-detail class, will break everything")
       throw new Error('NOT_IMPLEMENTED')
     }
     const date = parseDate($(el).find('td[class="date"]').text())
-    const amount = normalizePrice($(el).find('td[class="mutuelle"]').text())
+    const masterAmount = normalizePrice($(el).find('td[class="depense hidden-xs"]').text())
+    const masterSocialSecuAmount = normalizePrice(
+      $(el).find('td[class="secu hidden-xs"]').text())
+    const groupAmount = normalizePrice($(el).find('td[class="mutuelle"]').text())
     const htmlUrl = baseUrl + $(el).next('tr').find('a[title="Imprimer"]').attr('href')
     const id = htmlUrl.match(/\d*?$/)[0]
-    console.log('htmlurl', htmlUrl)
-    console.log($(el).html())
+
+    // Scrape Metadatas of each sub bill
+    const subs = Array.from($(el).next('tr').find('tbody tr[class="ligne-titre"]'))
+    const subsLinesMetadatas = []
+    for (subIndex=0; subIndex<subs.length; subIndex++) {
+      subsLinesMetadatas.push(parseSubline($, subs[subIndex]))
+      log('info', `Found subline number ${subIndex} of master ${index}`)
+    }
 
     // If a pdf matching by id is found, set it as fileurl else generate a pdf from html
     const matchingPdf = pdfList.find(obj => obj.id === id)
     let fileurl = ''
     let filestream = null
-    if (matchingPdf && false) { // TEST CASE TODO REMOVE false
+    if (matchingPdf) {
       log('info', 'Found matching this masterline with a pdf')
       fileurl = matchingPdf.fileurl
     } else {
-      log('info', 'No pdf match this line, generating one myself')
-      const $htmlToPdf = await request(htmlUrl)
+      log('info', 'No pdf match this  masterline, generating one myself')
+      const $htmlToPdf = generateHtmlPdf(formatFrenchDate(date),
+                                         masterAmount,
+                                         masterSocialSecuAmount,
+                                         groupAmount,
+                                         subsLinesMetadatas
+                                        )
       filestream = generatePDF($htmlToPdf, htmlUrl)
     }
 
-    // Scrape each sub bill
-    $(el).next('tr').find('tbody tr[class="ligne-titre"]').each((subIndex, subEl) => {
-      let bill = parseSubline($, subEl)
-      log('info', `Found subline number ${subIndex} of master ${index}`)
-      console.log($(subEl).html())
-      bill = {
-        ...bill,
+    // Create bill for each subline
+    for (let sub of subsLinesMetadatas) {
+      let bill = {
+        originalDate: sub.originalDate,
+        beneficiary: sub.beneficiary,
+        subtype: sub.subtype,
+        originalAmount: sub.originalAmount,
+        socialSecurityRefund: sub.socialSecurityRefund,
+        amount: sub.amount,
         vendor: 'EoviMCD',
         type: 'health_costs',
         currency: '€',
         isRefund: true,
         isThirdPartyPayer: false,
-        filename: formatDate(date) + `_${amount}€_EoviMCD.pdf`,
+        filename: formatDate(date) + `_${groupAmount}€_EoviMCD.pdf`,
         date,
-        amount
+        groupAmount
       }
       if (fileurl !== '') {
-        bill = {...bill, fileurl}
+        bill.fileurl = fileurl
       } else {
-        bill = {...bill, filestream}
+        bill.filestream = filestream
       }
       bills.push(bill)
-    })
-
-  })
+    }
+  }
   return bills
 }
 
 function parseSubline($, subEl) {
   const originalDate = parseDate($(subEl).find('td[data-column-header="Date"]').text())
   const beneficiary = $(subEl).find('td[data-column-header="Pour"]').text()
-  const originalAmount = normalizePrice($(subEl).find('td[data-column-header="Mutuelle"]').text())
+  const subtype = $(subEl).find('td[data-column-header="Nature"]').text()
+  const originalAmount = normalizePrice(
+      $(subEl).find('td[data-column-header="Dépense"]').text())
   const socialSecurityRefund = normalizePrice(
     $(subEl).find('td[data-column-header="Sécurité Sociale"]').text())
-  //TODO can scrape subtype, with medical infos
+  const amount = normalizePrice(
+    $(subEl).find('td[data-column-header="Mutuelle"]').text())
+
   return {
     originalDate,
     beneficiary,
+    subtype,
     originalAmount,
-    socialSecurityRefund
-         }
+    socialSecurityRefund,
+    amount
+  }
 }
 
 function scrapePdfList($) {
@@ -159,10 +181,11 @@ function scrapePdfList($) {
 
 function generatePDF($, url) {
   let doc = createCozyPDFDocument(
-    'Généré automatiquement par le connecteur Eovi MCD Mutuelle depuis la page',
-    url
+    'Généré automatiquement par le connecteur Eovi MCD Mutuelle',
+    '' // empty url
   )
-  htmlToPDF($, doc, $('body'), {})
+  htmlToPDF($, doc, $('body'),
+            {baseURL: 'https://www.espace-particuliers.eovi-mcd-mutuelle.fr/'})
   doc.end()
   return doc
 }
@@ -178,7 +201,7 @@ function parseDate(text) {
   return new Date(y, m - 1, d)
 }
 
-// Convert a Date object to a french date string
+// Convert a Date object to a ISO date string
 function formatDate(date) {
   let year = date.getFullYear()
   let month = date.getMonth() + 1
@@ -190,4 +213,47 @@ function formatDate(date) {
     day = '0' + day
   }
   return `${year}-${month}-${day}`
+}
+
+// Convert a Date object to a french date string
+function formatFrenchDate(date) {
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+  if (month < 10) {
+    month = '0' + month
+  }
+  if (day < 10) {
+    day = '0' + day
+  }
+  return `${day}/${month}/${year}`
+}
+
+function generateHtmlPdf(niceDate, masterAmount, masterSocialSecuAmount,
+                         groupAmount, subsLinesMetadatas) {
+  let htmlPdf = '<body>'
+  // First table section
+  htmlPdf += `<p><b>Votre remboursement du ${niceDate} :\n&nbsp</b></p>`
+  htmlPdf += '<table style="width:10px">' //dummy mandatory width
+  htmlPdf += '<tr><th><b>Date</b></th><th><b>Dépense totale</b></th><th><b>Remboursement Sécu</b></th><th><b>Remboursement Mutuelle</b></th></tr>'
+  htmlPdf += `<tr><th>${niceDate}</th><th>${masterAmount} €</th><th>${masterSocialSecuAmount} €</th><th>${groupAmount} €</th></tr>`
+  htmlPdf += '</table>'
+  // Second table section
+  htmlPdf += '<p><b>&nbsp\nDétail des soins :\n&nbsp</b></p>'
+  htmlPdf += '<table style="width:10px">' //dummy mandatory width
+  htmlPdf += '<tr><th><b>Date</b></th><th><b>Nature</b></th><th><b>Dépense totale</b></th><th><b>Remboursement Sécu</b></th><th><b>Remboursement Mutuelle</b></th></tr>'
+  for (let sub of subsLinesMetadatas) {
+    const niceOriginalDate = formatFrenchDate(sub.originalDate)
+    htmlPdf += `<tr><th>${niceOriginalDate}</th><th>${sub.subtype}</th><th>${sub.originalAmount} €</th><th>${sub.socialSecurityRefund} €</th><th>${sub.amount} €</th></tr>`
+  }
+  htmlPdf += '</table>'
+  // Third section
+  htmlPdf += '<p><b>&nbsp\nObtenez le document original Eovi-MCD</b></p>'
+  htmlPdf += '<p>&nbsp\nPour obtenir le document original Eovi-MCD, vous devez activer le relevé de prestations numériques sur votre espace personnel Eovi-MCD.\n</p>'
+  htmlPdf += '<p>&nbsp\nDans votre espace allez dans "Ma santé et mes démarches", "Relevés de prestations" et activez les relevés de prestations numériques.\n</p>'
+  htmlPdf += '<p>&nbsp\nUne fois activé, vous pouvez supprimer les pdfs Eovi-MCD de votre cozy, ils seront remplacés par les originaux au prochain lancement du connecteur.</p>'
+  htmlPdf += '<p>&nbsp\nPour vous connecter :\n&nbsp<a href="/login">https://www.espace-particuliers.eovi-mcd-mutuelle.fr/login</a></p>'
+  htmlPdf += '</body>'
+
+  return cheerio.load(htmlPdf)
 }
